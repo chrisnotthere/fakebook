b634bd47682cbd4aecce90acc52c8daad567408e
chris <mr.chrisnash@gmail.com>
2022-01-17 16:44:24 -0800

    setup

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,27 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config()
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const helmet = require('helmet');
+const express = require('express');
+const path = require('path');
+// const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Set up mongoose connection
+require('./config/mongoConfig');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+app.use(bodyParser.json())
+app.use(compression());   // compress all routes
+app.use(helmet());        // helps protet against vulnerabilites
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
