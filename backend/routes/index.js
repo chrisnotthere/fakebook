fa45a060f223c36029e7ad856ccf944ebe765523
chris <mr.chrisnash@gmail.com>
2022-01-17 21:42:17 -0800

    playing around with routing

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var Post = require("../models/post");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  res.json( {text: "hello from the server"} )
+
 });
 
 module.exports = router;
