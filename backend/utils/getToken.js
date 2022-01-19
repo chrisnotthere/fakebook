110910d14291ebe9006701c60efe58a863eb3f19
chris <mr.chrisnash@gmail.com>
2022-01-19 10:47:07 -0800

    JWT setup

---
+++ backend/utils/getToken.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+
+const getTokenData = (req, res, next) => {
+  if (!req.user) {
+    const jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+    const payload = jwt.verify(jwtFromRequest(req), process.env.JWT_KEY);
+    req.payload = payload;
+  } else {
+    req.payload = req.user;
+  }
+  next();
+};
+
+module.exports = getTokenData;
