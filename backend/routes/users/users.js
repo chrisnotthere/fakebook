c641253dd57e1a5a1a73f8c8cf03a4fe3becc313
chris <mr.chrisnash@gmail.com>
2022-01-18 19:52:00 -0800

    login appears to be working

--- backend/routes/users/users.js
+++ backend/routes/users/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 var User = require("../../models/user");
 const generateJWT = require('../../utils/generateJWT');
+const bcrypt = require("bcryptjs");
 
 const { body, validationResult } = require("express-validator");
 const friendsRouter = require('./friends');
@@ -64,11 +65,72 @@ router.post('/signup',
   }
 );
 
-
 /* POST login */
-router.post('/login', function (req, res, next) {
-  res.json({ message: "POST login - not implemented" })
-});
+router.post('/login',
+  //validate user input
+  body("email")
+    .trim()
+    .isEmail()
+    .escape()
+    .withMessage("Must enter valid email."),
+  body("password")
+    .trim()
+    .isLength({ min: 6 })
+    .escape()
+    .withMessage("Password must be at least 6 characters long."),
+
+
+  async (req, res, next) => {
+    // extract the validation errors from request.
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // if error(s), show error
+      console.log(errors);
+      return res.status(400).json({
+        message: "Oops! Something went wrong.",
+        errors: errors.array()
+      });
+    } else {
+      // validation passed, proceed to check credentials
+      let { email, password } = req.body;
+      //check if email is in DB
+      User.findOne({ email: email }, (err, user) => {
+        if (err) {
+          console.log('--there was an error--');
+          return res.status(401).json({ message: "there was an error" })
+        }
+        if (!user) {
+          console.log('--incorrect email--');
+          return res.status(401).json({ message: "incorrect email" })
+        }
+    
+        bcrypt.compare(password, user.password, (err, response) => {
+          if (err){
+            // handle error
+            console.log('--there was an error!--');
+            return res.status(500).json({ message: "there was an error!" })
+          }
+          if (response) {
+            //passwords match, create token and send to client
+            console.log('--passwords match!--');
+            const token = generateJWT(user); 
+
+            return res.status(200).json({
+              message: "Login success!",
+              token,
+              user,
+            })        
+          } else {
+            // passwords do not match!
+            console.log('--passwords do not match!--');
+            return res.status(401).json({ message: "passwords do not match!" })
+          }
+        });
+      });
+    }
+  }
+);
 
 /* POST login test account */
 router.post('/testuser', function (req, res, next) {
