0897c881857e7cc261256190951df7bc6dffe28f
chris <mr.chrisnash@gmail.com>
2022-01-19 10:46:52 -0800

    facebook login setup

--- backend/config/facebookConfig.js
+++ backend/config/facebookConfig.js
@@ -2,30 +2,39 @@ const FacebookTokenStrategy = require("passport-facebook-token");
 const passport = require("passport");
 const User = require("../models/user");
 
-const bcrypt = require("bcryptjs");
-
+/////////////////////////UNFINISHED/////////////////////////
 //setting up FacebookTokenStrategy
-function initialize(passport) {
+module.exports = new FacebookTokenStrategy(
+  {
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    fbGraphVersion: "v3.0",
+  },
+  function (accessToken, refreshToken, profile, done) {
+    User.findOrCreate(
+      { facebookId: profile.id },
+      {
+        firstName: profile._json.first_name,
+        lastName: profile._json.last_name,
+        email: profile._json.email,
+        picture: profile.photos[0].value,
+      },
+      function (error, user) {
+        return done(error, user);
+      }
+    );
+  }
+);
 
-  passport.use(
-    new FacebookTokenStrategy((username, password, done) => {
+// https://stackoverflow.com/questions/27637609/understanding-passport-serialize-deserialize
+passport.serializeUser(function (user, done) {
+  console.log(`--serialize user--`);
+  done(null, user.id);
+});
 
-    })
-  );
-  
-  // save user.id in cookie/session
-  passport.serializeUser(function (user, done) {
-    console.log(`serialize user`);
-    done(null, user.id);
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    console.log(`--deserialize user--`);
+    done(err, user);
   });
-  
-  // user.id is used to find the user
-  passport.deserializeUser(function (id, done) {
-    User.findById(id, function (err, user) {
-      console.log(`deserialize user`);
-      done(err, user);
-    });
-  });
-}
-
-module.exports = initialize;
+});
