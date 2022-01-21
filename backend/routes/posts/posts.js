de3d7357484e5bf10e83568e6a54b892962e82db
chris <mr.chrisnash@gmail.com>
2022-01-21 10:28:33 -0800

    create a new post working

--- backend/routes/posts/posts.js
+++ backend/routes/posts/posts.js
@@ -6,6 +6,7 @@ var User = require("../../models/user");
 var Comment = require("../../models/comment");
 const passport = require('passport');
 const getToken = require('../../utils/getToken')
+const { body, validationResult } = require("express-validator");
 
 router.use('/comments', commentsRouter);
 
@@ -17,11 +18,11 @@ router.get('/', async (req, res, next) => {
   try {
     // list of timeline posts - (self posts and posts of friends)
     const currentUser = await User.findById(req.payload.id);
-    const timelinePosts = await   Post.find({ user: [req.payload.id, ...currentUser.friends] })
-    .sort("-timestamp")
-    .populate({ path: 'user', select: 'firstName lastName' })
-    .populate({ path: 'comments', select: 'user text likes' })
-    .populate({ path:'likes', select: 'firstName lastName' })
+    const timelinePosts = await Post.find({ user: [req.payload.id, ...currentUser.friends] })
+      .sort("-timestamp")
+      .populate({ path: 'user', select: 'firstName lastName' })
+      .populate({ path: 'comments', select: 'user text likes' })
+      .populate({ path: 'likes', select: 'firstName lastName' })
 
     return res.status(200).json({ timelinePosts })
 
@@ -31,27 +32,47 @@ router.get('/', async (req, res, next) => {
 });
 
 /* POST create new post */
-router.post('/', function(req, res, next) {
-  res.json({ message: "POST create new post" })
-});
+router.post('/',
+  body('text', 'Your post must contain text.').trim().isLength({ min: 1 }),
+  async (req, res, next) => {
+    // check that the post contains text
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const postText = req.body.text;
+      const newPost = new Post({ user: req.payload.id, text: postText })
+      const savedPost = await newPost.save();
+
+      // check if saved post is now in the db
+      const foundPost = await Post.findById(savedPost._id);
+      if (foundPost) return res.status(201).json({ message: 'Post saved!', foundPost });
+      else res.status(500).json({ message: "Oops, something went wrong." });
+
+    } catch (error) {
+      return res.status(500).json({ message: "Oops, something went wrong.", error: error.message });
+    }
+  });
 
 /* GET specific post */
-router.get('/:id', function(req, res, next) {
+router.get('/:id', async (req, res, next) => {
   res.json({ message: "POST get a single post" })
 });
 
 /* PUT update post */
-router.put('/:id', function(req, res, next) {
+router.put('/:id', async (req, res, next) => {
   res.json({ message: "PUT edit post" })
 });
 
 /* PUT like/unlike toggle */
-router.put('/:id/like', function(req, res, next) {
+router.put('/:id/like', async (req, res, next) => {
   res.json({ message: "PUT toggle like post" })
 });
 
 /* DELETE post */
-router.delete('/:id', function(req, res, next) {
+router.delete('/:id', async (req, res, next) => {
   res.json({ message: "DELETE post" })
 });
 
