40d84a760fc974d609f303073d28c66b8c4a9407
chris <mr.chrisnash@gmail.com>
2022-01-21 09:56:05 -0800

    get timeline posts

--- backend/routes/posts/posts.js
+++ backend/routes/posts/posts.js
@@ -1,35 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const commentsRouter = require('./comments');
 var Post = require("../../models/post");
 var User = require("../../models/user");
 var Comment = require("../../models/comment");
-
-const commentsRouter = require('./comments');
+const passport = require('passport');
+const getToken = require('../../utils/getToken')
 
 router.use('/comments', commentsRouter);
 
-/* GET posts for timeline */
-router.get('/', function(req, res, next) {
+router.use(passport.authenticate(["jwt", "facebook-token"], { session: false }));
+router.use(getToken);
 
-  // list of all posts    **edit this to only get posts from self and friends**
-  Post.find()
-  .sort([["date", "ascending"]])
-  .populate({ path: 'user', select: 'firstName lastName' })
-  .populate({ path: 'comments', select: 'user text' })
-  .populate({
-    path: "comments",
-    select: "text likes",
-  })  
-  // .populate('likes')
-  .exec(function (err, results) {
-    if (err) {
-      return next(err);
-    }
-    //Successful, so send data
-    res.status(200).json({ results })
-  });
-  // next();
+/* GET timeline posts */
+router.get('/', async (req, res, next) => {
+  try {
+    // list of timeline posts - (self posts and posts of friends)
+    const currentUser = await User.findById(req.payload.id);
+    const timelinePosts = await   Post.find({ user: [req.payload.id, ...currentUser.friends] })
+    .sort("-timestamp")
+    .populate({ path: 'user', select: 'firstName lastName' })
+    .populate({ path: 'comments', select: 'user text likes' })
+    .populate({ path:'likes', select: 'firstName lastName' })
 
+    return res.status(200).json({ timelinePosts })
+
+  } catch (error) {
+    return res.status(500).json({ message: "Oops, something went wrong.", error: error.message });
+  }
 });
 
 /* POST create new post */
