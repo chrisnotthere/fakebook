4dbb226254a6b8887f43af9a70ee7dd95fa26b3c
chris <mr.chrisnash@gmail.com>
2022-01-24 10:33:52 -0800

    testing with axios

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,12 +1,27 @@
 import React, { useState } from "react";
 import "./post.css";
 import { MoreVert } from "@material-ui/icons";
-import { Users } from '../../testData';
+// import { Users } from '../../testData';
+import { useEffect } from "react";
+import axios from 'axios';
 
 function Post({ post }) {
   const [like, setLike] = useState(post.like);
   const [isLiked, setIsLiked] = useState(false);
+  const [user, setUser] = useState({});
+
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
+  useEffect(() => {
+    console.log('---render feed---')
+    const fetchTimeline = async () => {
+      // const res = await axios.get('posts/')
+      const res = await axios.get(`users/${post.user.id}`, { headers: {"Authorization" : `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZWJhMWY0M2ZjZTViZjgwOTkwYmYxMSIsImlhdCI6MTY0MzA0NDE0OTM3MywiZXhwIjoxNjQzMDQ0MjM1NzczfQ.jBwyWtuZeg0RoH8I7QU_oWlY7n6m1RAQUBjDCab5NRA`} });
+      console.log(res.data.user);
+      setUser(res.data.user)
+    }
+    fetchTimeline();
+  }, [])
 
   const likeHandler = () => {
     // console.log(e);
@@ -21,10 +36,10 @@ function Post({ post }) {
           <div className="postTopLeft">
             <img
               className="postProfileImg"
-              src={PF + Users.filter((user) => user.id === post.userId)[0].profilePicture}
-              alt="Bobby Smith"
+              src={process.env.REACT_APP_PUBLIC_FOLDER + '/person/' + user.picture}
+              alt={user.firstName}
             />
-            <span className="postUsername">{Users.filter((user) => user.id === post.userId)[0].username}</span>
+            <span className="postUsername">{user.firstName}</span>
             <span className="postDate">{post.date}</span>
           </div>
           <div className="postTopRight">
@@ -32,7 +47,7 @@ function Post({ post }) {
           </div>
         </div>
         <div className="postCenter">
-          <span className="postText">{post?.desc}</span>
+          <span className="postText">{post?.text}</span>
           <img className="postImg" src={PF+post.photo} alt='' />
         </div>
         <div className="postBottom">
