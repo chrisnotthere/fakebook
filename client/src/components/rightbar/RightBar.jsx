4403dc86605844844f713b9ecde4c126cd017eb0
chris <mr.chrisnash@gmail.com>
2022-01-23 19:58:35 -0800

    profile and dashboard pages

--- client/src/components/rightbar/RightBar.jsx
+++ client/src/components/rightbar/RightBar.jsx
@@ -3,27 +3,47 @@ import { Users } from '../../testData';
 import FriendRequest from "../friendrequest/FriendRequest";
 import Friend from "../friend/Friend";
 
-function RightBar() {
-  return (
-    <div className='rightBar'>
-      <div className="rightbarWrapper">
+function RightBar({ profile }) {
 
+  const DashRightbar = () => {
+    return (
+      <>
         <h4 className="rightbarTitle">Friend Requests</h4>
         <ul className="rightbarFriendRequests">
           {Users.map(u => (
             <FriendRequest key={u.id} user={u} />
           ))}
         </ul>
-
         <hr className="rightbarHr" />
-
         <h4 className="rightbarTitle">Friends</h4>
         <ul className="rightbarFriends">
           {Users.map(u => (
             <Friend key={u.id} user={u} />
           ))}
         </ul>
+      </>
+    );
+  };
 
+  const ProfileRightbar = () => {
+    return (
+      <>
+        <h4 className="rightbarTitle">Friends of Safak</h4>
+        <ul className="rightbarFriends">
+          {Users.map(u => (
+            <Friend key={u.id} user={u} />
+          ))}
+        </ul>
+      </>
+    )
+  }
+
+  return (
+    <div className='rightBar'>
+      <div className="rightbarWrapper">
+        {profile ? <ProfileRightbar /> : <DashRightbar />}
+        {/* <ProfileRightbar /> */}
+        {/* <DashRightbar /> */}
       </div>
     </div>
   );
