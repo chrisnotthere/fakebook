0c01284a67a285f92fd5edb7a2df163c9e49880c
chris <mr.chrisnash@gmail.com>
2022-01-24 10:33:52 -0800

    testing with axios

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -2,16 +2,32 @@ import "./feed.css";
 import PostForm from "../postform/PostForm";
 import Post from "../post/Post";
 import {Posts} from '../../testData'
+import { useState, useEffect } from "react";
+import axios from 'axios';
 
 function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    console.log('---render feed---')
+    const fetchTimeline = async () => {
+      // const res = await axios.get('posts/')
+      const res = await axios.get('posts/', { headers: {"Authorization" : `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZWJhMWY0M2ZjZTViZjgwOTkwYmYxMSIsImlhdCI6MTY0MzA0NDE0OTM3MywiZXhwIjoxNjQzMDQ0MjM1NzczfQ.jBwyWtuZeg0RoH8I7QU_oWlY7n6m1RAQUBjDCab5NRA`} });
+      console.log(res.data.timelinePosts);
+      setPosts(res.data.timelinePosts)
+    }
+    fetchTimeline();
+  }, [])
+
   return (
     <div className='feed'>
       <div className="feedWrapper">
         <PostForm />
-        {Posts.map((p) => (
-          <Post key={p.id} post={p} />
+        {posts.map((p) => (
+          <Post key={p._id} post={p} />
         ))}
-        {/* <Post /> */}
+
+
       </div>
     </div>
   );
