a45195cfcfcbcf258356f2ffd7ddbeb72b24d6b1
chris <mr.chrisnash@gmail.com>
2022-01-24 16:42:10 -0800

    login with localstorage

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -5,18 +5,20 @@ import { Posts } from '../../testData'
 import { useState, useEffect } from "react";
 import axios from 'axios';
 
-function Feed({ userid }) {
+function Feed({ user, setUser }) {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     console.log('---render feed---')
-    console.log('feed, user id ->' + userid);
+    // console.log('feed, user id ->' + userid);
+    console.log('feed, user ----->' + user);
+
 
     // show timeline posts if user is in the dashboard
     // show a specific person's posts if user is viewing person's profile
     const fetchTimeline = async () => {
-      const res = userid
-        ? await axios.get(`posts/${userid}`, { headers: { "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZWYwNjkzNjdjMzQ5MzJiYmU1ZmM0MCIsImlhdCI6MTY0MzA1NDk2ODQ3OSwiZXhwIjoxNjQzMDU1MDU0ODc5fQ.zNO1yEOkzUZhJMMA-n0BWVS2snsVcfBDAuHTyo4s9Sg` } })
+      const res = user.id
+        ? await axios.get(`posts/${user.id}`, { headers: { "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZWYwNjkzNjdjMzQ5MzJiYmU1ZmM0MCIsImlhdCI6MTY0MzA1NDk2ODQ3OSwiZXhwIjoxNjQzMDU1MDU0ODc5fQ.zNO1yEOkzUZhJMMA-n0BWVS2snsVcfBDAuHTyo4s9Sg` } })
         : await axios.get('posts/', { headers: { "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZWYwNjkzNjdjMzQ5MzJiYmU1ZmM0MCIsImlhdCI6MTY0MzA1NDk2ODQ3OSwiZXhwIjoxNjQzMDU1MDU0ODc5fQ.zNO1yEOkzUZhJMMA-n0BWVS2snsVcfBDAuHTyo4s9Sg` } });
       // console.log(res.data);
       const timelinePosts = res.data.timelinePosts;
@@ -25,11 +27,12 @@ function Feed({ userid }) {
         : setPosts(res.data.userPosts);
     }
     fetchTimeline();
-  }, [userid])
+  }, [user.id])
 
   return (
     <div className='feed'>
       <div className="feedWrapper">
+        <h3>{user ? `Welcome back, ${user.firstName}` : ''}</h3>
         <PostForm />
         {posts.map((p) => (
           <Post key={p._id} post={p} />
--- client/src/components/usernav/UserNav.jsx
+++ client/src/components/usernav/UserNav.jsx
@@ -1,15 +1,19 @@
 import "./usernav.css";
-import {RssFeed, People, GroupAdd} from '@material-ui/icons'
+import { RssFeed, People, GroupAdd } from '@material-ui/icons'
+import { Link } from "react-router-dom";
 
 function UserNav() {
   return (
     <div className="userNav">
       <div className="userNavWrapper">
         <ul className="userNavList">
-          <li className="userNavListItem">
-            <RssFeed className="userNavIcon" />
-            <span className="userNavListItemText">Feed</span>
-          </li>
+          <Link to={'/'} style={{textDecoration:'none', color:'inherit'}} >
+            <li className="userNavListItem">
+              <RssFeed className="userNavIcon" />
+              <span className="userNavListItemText">Feed</span>
+            </li>
+          </Link>
+
           <li className="userNavListItem">
             <People className="userNavIcon" />
             <span className="userNavListItemText">Friends</span>
@@ -20,7 +24,7 @@ function UserNav() {
           </li>
         </ul>
         {/* <button className="userNavButton">Show More</button> */}
-        <hr className="userNavHr"/>
+        <hr className="userNavHr" />
       </div>
     </div>
   );
