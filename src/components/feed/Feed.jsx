f5cc83bb373c0b2182c2edb744e8b5721191cdfb
chris <mr.chrisnash@gmail.com>
2022-01-25 15:25:10 -0800

    profile and timeline mostly working

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -8,7 +8,9 @@ import { useParams } from "react-router-dom";
 
 function Feed({ user, setUser }) {
   const [posts, setPosts] = useState([]);
+  const [postOwner, setPostOwner] = useState([]);
   const params = useParams();
+  const [profileUser, setProfileUser] = useState([]);
 
   // check to see if user is on homepage or profile page
   const isHome = (params) => {
@@ -19,17 +21,81 @@ function Feed({ user, setUser }) {
     return true;
   }
 
+  // check to see if user is on own profile page
+  const isCurrentUserProfile = (user, params) => {
+    if (user.id === params.id) {
+      return true
+    }
+    return false
+  }
+
+  //get profile user information, only if currentUser is looking at someone elses profile page
+  useEffect(() => {
+    const getProfileUser = async () => {
+      // only run if not on own profile page
+      if (!isCurrentUserProfile(user, params)) {
+        try {
+          const profileUser = await axios.get(`/users/${params.id}/`, { headers: { "Authorization": user.token } });
+          setProfileUser(profileUser.data.user);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    }
+    //??
+    if (!isHome(params)){
+      getProfileUser();
+    }
+
+  }, [])
+
+  //get posts
   useEffect(() => {
     console.log('---render feed---')
     // show timeline posts if user is in the dashboard
     // show a specific person's posts if user is viewing person's profile
-    const fetchTimeline = async () => {
-      const res = user.id
-        ? await axios.get(`posts/${user.id}`, { headers: { "Authorization": user.token } })
-        : await axios.get('posts/', { headers: { "Authorization": user.token } });
-      setPosts(res.data.userPosts)
+
+    const fetchPosts = async () => {
+      //if on dash -> pull timeline posts (GET /)
+      ///////////////////////////////////////////////////////////////////////////
+      // console.log(isHome(params)) // true when on dash
+      if (isHome(params)) {
+        try {
+
+
+          const res = await axios.get('posts/', { headers: { "Authorization": user.token } });
+          setPosts(res.data.timelinePosts)
+          console.log(res.data.timelinePosts)
+
+
+        } catch (err) {
+          console.log(err)
+        }
+      } else {
+        //if on profile page -> pull only that user's posts... (GET /:id)
+        try {
+          if (isCurrentUserProfile(user, params)) {
+            //if own profile use user.id
+            const res = await axios.get(`posts/${user.id}`, { headers: { "Authorization": user.token } })
+            // console.log(res.data)
+            setPosts(res.data.userPosts)
+
+          } else {
+            //if someone elses page use params
+            const res = await axios.get(`posts/${params.id}`, { headers: { "Authorization": user.token } })
+            // console.log(res.data)
+            setPosts(res.data.userPosts)
+          }
+        } catch (err) {
+          console.log(err)
+        }
+      }
+      // const res = user.id
+      //   ? await axios.get(`posts/${user.id}`, { headers: { "Authorization": user.token } })
+      //   : await axios.get('posts/', { headers: { "Authorization": user.token } });
+      // setPosts(res.data.userPosts)
     }
-    fetchTimeline();
+    fetchPosts();
   }, [user])
 
   return (
@@ -39,8 +105,8 @@ function Feed({ user, setUser }) {
         {/* hide 'create post' on profile page */}
         {isHome(params) && <PostForm user={user} setUser={setUser} />}
 
-        {posts.map((p) => (
-          <Post user={user} setUser={setUser} key={p._id} post={p} />
+        {posts?.map((p) => (
+          <Post user={user} setUser={setUser} key={p._id} post={p} profileUser={profileUser} postOwner={postOwner} />
         ))}
 
 
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -6,10 +6,12 @@ import "./profile.css";
 import { useState, useEffect } from "react";
 import axios from 'axios';
 import { useParams } from "react-router";
+import { ContactSupportOutlined } from "@material-ui/icons";
 
 function Profile({ user, setUser }) {
   // const [user, setUser] = useState({});
   const userid = useParams();
+  const [profileUser, setProfileUser] = useState([]);
 
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   // console.log('profile, userid -->', userid.id);
@@ -23,7 +25,35 @@ function Profile({ user, setUser }) {
   //   fetchUserProfile();
   // }, [])
 
-  // console.log(user)
+  // check to see if user is on own profile page
+  const isCurrentUserProfile = (user, params) => {
+    if (user.id === params.id) {
+      return true
+    }
+    return false
+  }
+
+  // console.log('userid.id', userid.id);
+
+  //get profile user information, only if currentUser is looking at someone elses profile page
+  useEffect(() => {
+    const getProfileUser = async () => {
+      // only run if not on own profile page
+      if (!isCurrentUserProfile(user, userid)) {
+        try {
+          const profileUser = await axios.get(`/users/${userid.id}/`, { headers: { "Authorization": user.token } });
+          setProfileUser(profileUser.data.user);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    }
+    getProfileUser();
+  }, [])
+
+
+  // console.log('user', user)
+  // console.log(isCurrentUserProfile(user, userid))
 
   return (
     <>
@@ -38,16 +68,34 @@ function Profile({ user, setUser }) {
                 src={user.coverPicture || PF + "/post/1.jpg"}
                 alt=""
               />
+              {/* pull from params if not logged in user */}
               <img
                 className="profileUserImg"
-                src={PF + '/person/' + user.picture}
+                src=
+                {isCurrentUserProfile(user, userid)
+                  ? PF + '/person/' + user.picture
+                  : PF + '/person/' + profileUser.picture
+                }
                 alt=""
               />
+
             </div>
             <div className="profileInfo">
-              <h4 className="profileInfoName">{user.firstName}</h4>
-              {/* <span className="profileInfoDesc">Hello my friends!</span> */}
-              <span className="profileInfoDesc">{user.about}</span>
+              {/* here too */}
+              <h4 className="profileInfoName">
+                {/* {user.firstName} */}
+                {isCurrentUserProfile(user, userid)
+                  ? user.firstName
+                  : profileUser.firstName
+                }
+              </h4>
+              <span className="profileInfoDesc">
+                {/* {user.about} */}
+                {isCurrentUserProfile(user, userid)
+                  ? user.about
+                  : profileUser.about
+                }
+              </span>
             </div>
           </div>
           <div className="profileRightBottom">
