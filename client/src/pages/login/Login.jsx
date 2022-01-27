4a8f7adfe32eb2ca55d9ef14afced20334cfc467
chris <mr.chrisnash@gmail.com>
2022-01-27 10:18:05 -0800

    errors now display on failed login attempt

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -11,42 +11,39 @@ function Login({ user, setUser }) {
 
   const handleLogin = (e, email, password) => {
     e.preventDefault();
-    console.log('clicked');
-    console.log(email, password);
 
     axios
-    .post("/auth/login", { email, password })
-    .then((result) => {
-      console.log('-----___-----' + result.data)
-      const user = {
-        email: result.data.user.email,
-        firstName: result.data.user.firstName,
-        lastName: result.data.user.lastName,
-        token: result.data.token.token,
-        id: result.data.user.id,
-        picture: result.data.user.picture,
-        about: result.data.user.about
-      };
-      axios.defaults.headers.common["Authorization"] =
-        result.data.token.token;
-      setUser(user);
-      setEmail("");
-      setPassword("");
-      navigate("/");
-    })
-    .catch((err) => {
-      if (err.response.data.errors) {
-        setErrors(err.response.data.errors);
-        setTimeout(() => {
-          setErrors([]);
-        }, 3000);
-      } else if (err.response.data.message) {
-        setErrors([...errors, { msg: err.response.data.message }]);
-        setTimeout(() => {
-          setErrors([]);
-        }, 3000);
-      }
-    });
+      .post("/auth/login", { email, password })
+      .then((result) => {
+        const user = {
+          email: result.data.user.email,
+          firstName: result.data.user.firstName,
+          lastName: result.data.user.lastName,
+          token: result.data.token.token,
+          id: result.data.user.id,
+          picture: result.data.user.picture,
+          about: result.data.user.about
+        };
+        axios.defaults.headers.common["Authorization"] =
+          result.data.token.token;
+        setUser(user);
+        setEmail("");
+        setPassword("");
+        navigate("/");
+      })
+      .catch((err) => {
+        if (err.response.data.errors) {
+          setErrors(err.response.data.errors);
+          setTimeout(() => {
+            setErrors([]);
+          }, 5000);
+        } else if (err.response.data.message) {
+          setErrors([...errors, { msg: err.response.data.message }]);
+          setTimeout(() => {
+            setErrors([]);
+          }, 5000);
+        }
+      });
   }
 
   return (
@@ -62,9 +59,18 @@ function Login({ user, setUser }) {
           <form className="loginBox" onSubmit={(e) => handleLogin(e, email, password)} >
             <input placeholder="Email" className="loginInput" type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
             <input placeholder="Password" className="loginInput" type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+            {errors
+              ? errors.map((error) => {
+                return (
+                  <span style={{ color: "red", display: "block", width: "100%" }}>
+                    {error.msg}
+                  </span>
+                );
+              })
+              : null}
             <button className="loginButton" >Log In</button>
             <span className="loginForgot">Forgot Password?</span>
-            <Link to={'/register'} style={{ textDecoration: 'none', color: 'inherit'}} >
+            <Link to={'/register'} style={{ textDecoration: 'none', color: 'inherit' }} >
               <button className="loginRegisterButton">Sign Up</button>
             </Link>
             {/* <button className="loginButton">Login with FaceBook</button> */}
