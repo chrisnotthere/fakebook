f40041f6696d850a3cf3a911da9c614ce06fe866
chris <mr.chrisnash@gmail.com>
2022-01-27 16:46:39 -0800

    cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import DashBoard from './pages/dashboard/DashBoard';
 import Login from './pages/login/Login';
 import Profile from './pages/profile/Profile';
 import Register from './pages/register/Register';
-import { BrowserRouter as Router, Routes, Route, useNavigate, } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import useLocalStorage from './utils/useLocalStorage';
 import MyFriends from './pages/myFriends/MyFriends';
 import FindFriends from './pages/findFriends/FindFriends';
@@ -13,7 +13,7 @@ const App = () => {
 
   return (
     <div >
-      {/* <Router> */}
+      <Router>
         <Routes>
           <Route path="/login" element={<Login user={user} setUser={setUser} />} />
           <Route path="/register" element={<Register user={user} setUser={setUser} />} />
@@ -22,7 +22,7 @@ const App = () => {
           <Route path="/" element={<DashBoard user={user} setUser={setUser} />} />
           <Route path="/:id" element={<Profile user={user} setUser={setUser} />} />
         </Routes>
-      {/* </Router> */}
+      </Router>
     </div>
   );
 }
