102fa113c68ec809d3720383969568bf1bd7659e
chris <mr.chrisnash@gmail.com>
2022-01-29 15:24:32 -0800

    fixing bugs

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { PostFormContainer } from "./styles/PostForm.styled";
+// import { useParams } from "react-router-dom";
 
 function PostForm({ user, setUser}) {
+  // const userid = useParams();
+  const [profileUser, setProfileUser] = useState([]);
   const [content, setContent] = useState('');
   let data;
 
@@ -22,10 +25,22 @@ function PostForm({ user, setUser}) {
     } catch (err) {}
   }
 
+  useEffect(() => {
+    const getProfileUser = async () => {
+      try {
+        const profileUser = await axios.get(`/users/${user.id}/`, { headers: { "Authorization": user.token } });
+        setProfileUser(profileUser.data.user);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getProfileUser();
+  }, [])
+
   return (
     <PostFormContainer>
       <div className="postformWrapper">
-
+        <h2 className="postformWelcome" >Welcome, {profileUser.firstName}</h2>
         <form className='postformForm' onSubmit={handleSubmit}>
           <legend>Create a post</legend>
 
