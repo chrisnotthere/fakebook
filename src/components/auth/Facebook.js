02c6a7e614c5e9157865a6caa0add24cb8c60a91
chris <mr.chrisnash@gmail.com>
2022-01-30 13:36:47 -0800

    newly registered users now have friend reqs populated

--- src/components/auth/Facebook.js
+++ src/components/auth/Facebook.js
@@ -1,3 +1,5 @@
+import { ContactSupportOutlined } from '@material-ui/icons';
+import axios from 'axios';
 import React from 'react';
 import FacebookLogin from 'react-facebook-login';
 import { useNavigate } from 'react-router-dom';
@@ -6,12 +8,12 @@ const Facebook = ({ user, setUser }) => {
   const navigate = useNavigate();
 
   const handleFBLogin = (accessToken) => {
-    const url = "/auth/facebook/token";
+    const url = '/auth/facebook/token';
     fetch(url, {
       method: 'POST',
       // credentials: 'include',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
         'Authorization': `Bearer ${accessToken}`
       }
     }).then((result) => {
@@ -28,33 +30,45 @@ const Facebook = ({ user, setUser }) => {
         profilePicUrl: data.user.profilePicUrl,
         facebookId: data.user.facebookId,
       };
-      // console.log(user);
+      console.log('user1', user);
       setUser(user);
-      navigate("/");
+      //populate friend requests THIS IS NOT WORKING
+      populateFriendRequests();
+      console.log('user2', user);
+      navigate('/');
     });
   };
 
   const componentClicked = () => console.log('clicked');
 
   const responseFacebook = response => {
-    console.log( 'facebook response', response);
+    console.log('facebook response', response);
     handleFBLogin(response.accessToken);
   };
+
+  const populateFriendRequests = async () => {
+    try {
+      await axios.post(`/users/friends/populate`);
+    } catch (err) {
+      console.log('error at popFriendReqs')
+      console.log(err)
+    }
+  }
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center' }}>
       <FacebookLogin
-        appId="663629001739255"
+        appId='663629001739255'
         // autoLoad={true}
-        fields="name,email,picture"
+        fields='name,email,picture'
         onClick={componentClicked}
         callback={responseFacebook}
-        icon="fa-facebook"
+        icon='fa-facebook'
         render={(renderProps) => (
           <button
             onClick={renderProps.onClick}
-            variant="contained"
-            color="secondary"
+            variant='contained'
+            color='secondary'
           >
             Log in with Facebook
           </button>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,17 +4,17 @@ import { useNavigate } from 'react-router-dom';
 import { Signup } from '../components/styles/Register.styled';
 
 function Register({ user, setUser }) {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [errors, setErrors] = useState([]);
   const navigate = useNavigate()
 
   const handleSignUp = (e, firstName, lastName, email, password) => {
     e.preventDefault()
     axios
-      .post("/auth/signup", { firstName, lastName, email, password })
+      .post('/auth/signup', { firstName, lastName, email, password })
       .then((result) => {
         console.log('result', result)
         const newUser = {
@@ -26,10 +26,15 @@ function Register({ user, setUser }) {
           picture: result.data.user.picture,
           about: '',
         };
-        axios.defaults.headers.common["Authorization"] =
+
+        axios.defaults.headers.common['Authorization'] =
           result.data.token.token;
         setUser(newUser);
-        populateFriends();
+
+        //populate friend requests
+        populateFriendRequests();
+        // //add 'FakeBook Official' account to friends list
+        // addFriend();
         setEmail('');
         setPassword('');
         setFirstName('');
@@ -52,35 +57,47 @@ function Register({ user, setUser }) {
       });
   }
 
-  const populateFriends = () => {
-    
+  const populateFriendRequests = async () => {
+    try {
+      await axios.post(`/users/friends/populate`);
+    } catch (err) {
+      console.log(err)
+    }
   }
+
+  // const addFriend = async () => {
+  //   try {
+  //     await axios.post(`/users/friends/addfriend`);
+  //   } catch (err) {
+  //     console.log(err)
+  //   }
+  // }
 
   return (
     <Signup>
-      <div className="signupWrapper">
-        <div className="signupLeft">
-          <h3 className="signupLogo">FakeBook</h3>
-          <span className="signupDesc">
+      <div className='signupWrapper'>
+        <div className='signupLeft'>
+          <h3 className='signupLogo'>FakeBook</h3>
+          <span className='signupDesc'>
             Connect with friends and stuff...
           </span>
         </div>
-        <div className="signupRight">
-          <form className="signupBox" onClick={(e) => handleSignUp(e, firstName, lastName, email, password)}>
-            <input placeholder="Email" type='email' className="signupInput" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <input placeholder="Password" type='password' className="signupInput" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <input placeholder="First Name" className="signupInput" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-            <input placeholder="Last Name" className="signupInput" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+        <div className='signupRight'>
+          <form className='signupBox' onClick={(e) => handleSignUp(e, firstName, lastName, email, password)}>
+            <input placeholder='Email' type='email' className='signupInput' value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input placeholder='Password' type='password' className='signupInput' value={password} onChange={(e) => setPassword(e.target.value)} />
+            <input placeholder='First Name' className='signupInput' value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+            <input placeholder='Last Name' className='signupInput' value={lastName} onChange={(e) => setLastName(e.target.value)} />
             {errors
               ? errors.map((error) => {
                 return (
-                  <span style={{ color: "red", display: "block", width: "100%" }}>
+                  <span style={{ color: 'red', display: 'block', width: '100%' }}>
                     {error.msg}
                   </span>
                 );
               })
               : null}
-            <button className="button" >Sign Up</button>
+            <button className='button' >Sign Up</button>
           </form>
         </div>
       </div>
