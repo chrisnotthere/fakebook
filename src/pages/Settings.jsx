d8babf61592b41b9d293f81eec30cd120a68783c
chris <mr.chrisnash@gmail.com>
2022-01-31 10:18:04 -0800

    profile settings page

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import UserNav from "../components/UserNav";
+import { SettingsContainer } from '../components/styles/Settings.styled'
+import { useNavigate } from 'react-router-dom';
+
+function Settings({ user, setUser }) {
+
+
+  return (
+    <>
+      <NavBar user={user} setUser={setUser} />
+      <SettingsContainer>
+        <UserNav user={user} setUser={setUser} />
+        <div className='settingsWrapper' >
+          <h2>Not implemented.</h2>
+          <p>TODO: edit profile picture, cover picture, and about/description</p>
+        </div>
+      </SettingsContainer>
+    </>
+  );
+}
+
+export default Settings;
