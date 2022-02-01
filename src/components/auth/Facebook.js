67cf44bd7e44604674140cabc9658e1ea4bcc3a2
chris <mr.chrisnash@gmail.com>
2022-01-31 16:03:16 -0800

    after signing in with facebook, friend requests are populated if user has no friend/friendReqs

--- src/components/auth/Facebook.js
+++ src/components/auth/Facebook.js
@@ -1,5 +1,3 @@
-import { ContactSupportOutlined } from '@material-ui/icons';
-import axios from 'axios';
 import React from 'react';
 import FacebookLogin from 'react-facebook-login';
 import { useNavigate } from 'react-router-dom';
@@ -17,24 +15,19 @@ const Facebook = ({ user, setUser }) => {
         'Authorization': `Bearer ${accessToken}`
       }
     }).then((result) => {
-      // console.log(result);
       return result.json();
     }).then(function (data) {
       console.log('api response', data);
       const user = {
         email: data.user.email,
-        firstName: data.user.first_name,
-        lastName: data.user.last_name,
+        firstName: data.user.firstName,
+        lastName: data.user.lastName,
         token: `Bearer ${accessToken}`,
         id: data.user.id,
-        profilePicUrl: data.user.profilePicUrl,
+        picture: data.user.picture,
         facebookId: data.user.facebookId,
       };
-      console.log('user1', user);
       setUser(user);
-      //populate friend requests THIS IS NOT WORKING ////
-      populateFriendRequests();
-      console.log('user2', user);
       navigate('/');
     });
   };
@@ -45,15 +38,6 @@ const Facebook = ({ user, setUser }) => {
     console.log('facebook response', response);
     handleFBLogin(response.accessToken);
   };
-
-  const populateFriendRequests = async () => {
-    try {
-      await axios.post(`/users/friends/populate`);
-    } catch (err) {
-      console.log('error at popFriendReqs')
-      console.log(err)
-    }
-  }
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center' }}>
