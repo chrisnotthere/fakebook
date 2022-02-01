b0b1f129f12cce76a18b0331ba04a68e28391baa
chris <mr.chrisnash@gmail.com>
2022-01-31 16:03:16 -0800

    after signing in with facebook, friend requests are populated if user has no friend/friendReqs

--- client/src/pages/DashBoard.jsx
+++ client/src/pages/DashBoard.jsx
@@ -4,11 +4,36 @@ import UserNav from '../components/UserNav';
 import Feed from '../components/Feed';
 import RightBar from '../components/RightBar';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function DashBoard({ user, setUser }) {
   const navigate = useNavigate();
 
-  // send to login if there is no user in localstorage
+  // populate facebook users friendRequests
+  useEffect(() => {
+
+    const populateFriendRequests = async () => {
+      console.log('user.id', user.id)
+      try {
+        const api = await axios.get(`/users/${user.id}`)
+        const currentUser = api.data.user;
+
+        // populate friendRequests if user has no friends and no friend requests
+        if (((!currentUser.friends.length > 0) && (!currentUser.friendRequests.length > 0))) {
+          await axios.post(`/users/friends/populate`);
+          window.location.reload();
+        }
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    // only run when user has a facebookId
+    if (!!user.facebookId) {
+      populateFriendRequests()
+    }
+  }, []);
+
+  // send to login if there is no 'user' in localstorage
   useEffect(() => {
     if (!user) {
       navigate('/login')
