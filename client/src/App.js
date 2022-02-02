12a8f27a2f6361505e1f69e390899c606cdd4a73
chris <mr.chrisnash@gmail.com>
2022-02-02 08:29:41 -0800

    deploy prep

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import DashBoard from './pages/DashBoard';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
 import Register from './pages/Register';
-import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, HashRouter, } from 'react-router-dom';
 import useLocalStorage from './utils/useLocalStorage';
 import MyFriends from './pages/MyFriends';
 import FindFriends from './pages/FindFriends';
@@ -14,9 +14,11 @@ import { theme } from './components/styles/Theme';
 const App = () => {
   const [user, setUser] = useLocalStorage('user', '');
 
+  // using HashRouter instead of Router so the page doesnt crash on refresh on gh-pages
+
   return (
     <ThemeProvider theme={theme} >
-      <Router>
+      <HashRouter>
         <Routes>
           <Route path="/login" element={<Login user={user} setUser={setUser} />} />
           <Route path="/register" element={<Register user={user} setUser={setUser} />} />
@@ -26,7 +28,7 @@ const App = () => {
           <Route path="/" element={<DashBoard user={user} setUser={setUser} />} />
           <Route path="/:id" element={<Profile user={user} setUser={setUser} />} />
         </Routes>
-      </Router>
+      </HashRouter>
     </ThemeProvider>
   );
 }
