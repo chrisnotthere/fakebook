1e4d4a024bc137ce86eea6d171e98e23af62a3b0
chris <mr.chrisnash@gmail.com>
2022-02-01 17:22:53 -0800

    deploy

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 const config = axios.create({
-  baseURL: "https://https://fakebook-mernapp.herokuapp.com/",
+  baseURL: "https://fakebook-mernapp.herokuapp.com/",
 });
 
 export default config;
