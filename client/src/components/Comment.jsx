ba672a2680af963575180c150e19a6b31e570b62
chris <mr.chrisnash@gmail.com>
2022-02-21 15:31:40 -0800

    comments now link to profile

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -2,6 +2,7 @@ import axios from '../utils/axios'
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { CommentContainer } from "./styles/Comment.styled";
+import { Link } from 'react-router-dom';
 
 function Comment({ comment, post, user }) {
   const [commentUser, setCommentUser] = useState();
@@ -33,14 +34,20 @@ function Comment({ comment, post, user }) {
       <div className="comment">
         <div className="commentWrapper">
 
-          <div className="commentPicture">
-            <img src={commentUser?.picture} alt="" />
-          </div>
-          
-          <div className="commentContent">
-            <div className="commentUsername">
-              {commentUser?.firstName + ' ' + commentUser?.lastName}
+          <Link to={`${commentUser?.id}`}>
+            <div className="commentPicture">
+              <img src={commentUser?.picture} alt="" />
             </div>
+          </Link>
+
+          <div className="commentContent">
+
+            <Link to={`${commentUser?.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+              <div className="commentUsername">
+                {commentUser?.firstName + ' ' + commentUser?.lastName}
+              </div>
+            </Link>
+
             <div className="commentText">
               {comment.text}
             </div>
